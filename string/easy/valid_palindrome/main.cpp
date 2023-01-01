45dc37a1162a15d19f40c6170bf383926394dd1f
bodja_n <bs@mt11.su>
2023-01-02 00:56:55 +0300

    valid palindrome

---
+++ string/easy/valid_palindrome/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+class Solution {
+public:
+    bool isPalindrome(std::string s) {
+        if (s.empty()) {
+            return true;
+        }
+        std::string explanation;
+        for (auto c : s) {
+            if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+                explanation.push_back(std::tolower(c));
+            }
+        }
+        size_t i = 0;
+        size_t j = explanation.size()-1;
+        for (; i < j; ++i, --j) {
+            if (explanation[i] != explanation[j]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main() {
+    std::string str{"a"};
+    Solution s;
+    std::cout << s.isPalindrome(str) << std::endl;
+    return 0;
+}
